ca9c83427f12780e61222921cc33830a95011cbb
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-02-15 13:29:46 +0300

    added extension for double

---
+++ WeatherApp/WeatherApp/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherApp
+//
+//  Created by Bartuğ Kaan Çelebi on 15.02.2024.
+//
+
+import Foundation
+
+extension Double {
+  func rounDouble() -> String{
+    return String(format: "%.0f", self)
+  }
+}
